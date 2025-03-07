109033d3e78fb61ac352e1b2cf8ae8de6bfafa0d
Goutham-AR <argouthu8400@gmail.com>
2025-03-07 10:33:24 +0530

    remove imports

--- src/generateTestForFile/app.ts
+++ src/generateTestForFile/app.ts
@@ -4,14 +4,8 @@ import { encoding_for_model } from "@dqbd/tiktoken";
 import fs from "fs";
 import { AppError } from "../lib/errors";
 import {
-  findImportSymbols,
-  getDefinitionsOfImportSymbols,
-  Import,
-} from "../lib/utils";
-import {
   GeneratePromptData,
   loadGeneratePrompt,
-  loadImportsText,
 } from "../lib/prompts/loadPrompts";
 import { IApp } from "../lib/app";
 
@@ -33,49 +27,30 @@ export class GenerateApp extends IApp {
         `package.json not found in ${workspaceFolders[0].uri.fsPath}`,
       );
     }
-    const packageJsonContent = fs.readFileSync(packageJsonPath, "utf8");
 
     const filepath = document.fileName;
-    const importSymbols = await findImportSymbols(document);
-    const definitions = await getDefinitionsOfImportSymbols(
-      importSymbols,
-      document,
-    );
-    const localDefinitions = definitions.filter(
-      (def) => def.locations.length > 0,
-    );
-    const imports: Import[] = localDefinitions.map((def) => ({
-      path: def.path,
-      code: def.locations[0].text,
-    }));
     const language = document.languageId;
-    const importsPrompt = loadImportsText({ imports });
     let promptData: GeneratePromptData = {
       language,
       sourceFileName: path.basename(filepath),
       sourceFileContent: sourceCode,
       testFramework: "jest",
-      importedContent: importsPrompt,
-      projectInfoFile: packageJsonContent,
-      projectInfoFileName: "package.json",
+      importedContent: "",
+      projectInfoFile: "",
+      projectInfoFileName: "",
     };
     let prompt = loadGeneratePrompt(promptData);
+    console.log(prompt);
     const tokenizer = encoding_for_model("gpt-3.5-turbo");
     const encoding = tokenizer.encode(prompt);
     if (encoding.length >= this._contextLength) {
-      this._showWarning(
-        "context limit exceeded, removing imports from the prompt",
-      );
-      promptData.importedContent = undefined;
-      prompt = loadGeneratePrompt(promptData);
-      if (tokenizer.encode(prompt).length >= this._contextLength) {
-        this._showWarning("context exceeded, the response might be inaccurate");
-      }
+      this._showWarning("context exceeded, the response might be inaccurate");
     }
     try {
       const response = await this._sendPromptStreaming(prompt);
       await this._writeStreamingOutput(response, language);
     } catch (e) {
+      console.error(e);
       this._showError(`llm server request failed: ${(e as Error).message}`);
     }
   }
